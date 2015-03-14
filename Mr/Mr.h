522dc6b40bc2d2937628601534711250e3be266e
fanfei <hzfanfei@corp.netease.com>
2015-03-14 19:23:51 +0800

    add Mr framework

---
+++ Mr/Mr.h
@@ -0,0 +1,19 @@
+//
+//  Mr.h
+//  Mr
+//
+//  Created by FanFamily on 15/3/14.
+//  Copyright (c) 2015年 familymrfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mr.
+FOUNDATION_EXPORT double MrVersionNumber;
+
+//! Project version string for Mr.
+FOUNDATION_EXPORT const unsigned char MrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mr/PublicHeader.h>
+
+
